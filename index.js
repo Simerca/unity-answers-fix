c081c648a5b62b68bce92aee5612ff24f18ca5e1
Simerca <contactsimerca@gmail.com>
2023-01-26 21:51:22 +0100

    Add This

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // Add more regexes here !
 document.body.innerHTML = document.body.innerHTML.replace(/t\$\$anonymous\$\$s/g, "this")
+                                                .replace(/T\$\$anonymous\$\$s/g, "This")
                                                 .replace(/C\$\$anonymous\$\$ld/g, "Child")
                                                 .replace(/c\$\$anonymous\$\$ld/g, "child")
                                                 .replace(/W\$\$anonymous\$\$le/g, "While")
