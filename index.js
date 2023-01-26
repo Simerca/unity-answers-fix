37e9ab489dc11b8a5d54c27c2d7283bbef4f3152
Simerca <contactsimerca@gmail.com>
2023-01-26 22:51:55 +0100

    Add highest

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.body.innerHTML = document.body.innerHTML.replace(/t\$\$anonymous\$\$s/g
                                                 .replace(/W\$\$anonymous\$\$le/g, "While")
                                                 .replace(/w\$\$anonymous\$\$le/g, "while")
                                                 .replace(/\$\$anonymous\$\$de/g, "hide")
+                                                .replace(/\$\$anonymous\$\$ghest/g, "highest")
                                                 .replace(/arc\$\$anonymous\$\$ve/g, "archive")
                                                 .replace(/\$\$anonymous\$\$athf/g, "Mathf")
                                                 .replace(/\$\$anonymous\$\$in/g, "Min")
