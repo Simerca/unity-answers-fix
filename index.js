bce7dc0057fb9759de589e5927520534d223b0cc
Jissaii <jc.puginier@gmail.com>
2023-01-27 11:22:37 +0100

    Update index.js
    Like this it does'nt need to recognize a whole word, you just replace "$$anonymous$$" with "hi". You won't need any updates unless "hi" is not the keyword anymore.

--- index.js
+++ index.js
@@ -2,21 +2,4 @@
  * @github Simerca
  */
 
-// Add more regexes here !
-document.body.innerHTML = document.body.innerHTML.replace(/t\$\$anonymous\$\$s/g, "this")
-                                                .replace(/T\$\$anonymous\$\$s/g, "This")
-                                                .replace(/t\$\$anonymous\$\$ng/g, "thing")
-                                                .replace(/t\$\$anonymous\$\$ngs/g, "things")
-                                                .replace(/T\$\$anonymous\$\$ng/g, "Thing")
-                                                .replace(/T\$\$anonymous\$\$ngs/g, "Things")
-                                                .replace(/C\$\$anonymous\$\$ld/g, "Child")
-                                                .replace(/c\$\$anonymous\$\$ld/g, "child")
-                                                .replace(/W\$\$anonymous\$\$le/g, "While")
-                                                .replace(/w\$\$anonymous\$\$le/g, "while")
-                                                .replace(/\$\$anonymous\$\$de/g, "hide")
-                                                .replace(/\$\$anonymous\$\$ghest/g, "highest")
-                                                .replace(/arc\$\$anonymous\$\$ve/g, "archive")
-                                                .replace(/\$\$anonymous\$\$athf/g, "Mathf")
-                                                .replace(/\$\$anonymous\$\$in/g, "Min")
-                                                .replace(/p\$\$anonymous\$\$cs/g, "phics") // Graphics
-                                                .replace(/\$\$anonymous\$\$erarchy/g, "hierarchy");
+document.body.innerHTML = document.body.innerHTML.replace(/\$\$anonymous\$\$/g, "hi");
